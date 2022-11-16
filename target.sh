c130c11a698967951a87803ef46a596354ceeb47
datyprjkt <118070774+datyprjkt@users.noreply.github.com>
2022-11-16 08:03:36 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 KERNEL_ROOTDIR=~/work/Ubuntu-Server/Ubuntu-Server/citrus
-KERNEL_DEFCONFIG=vendor/fury-perf_defconfig
+KERNEL_DEFCONFIG=vendor/codename_defconfig
 CLANG_ROOTDIR=~/work/Ubuntu-Server/Ubuntu-Server/proton-clang
 export KBUILD_BUILD_USER=DannPrjkt-CAF1.0
 export KBUILD_BUILD_HOST=DannPrjktCAF
