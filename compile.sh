a81957ffc59285759fc3f6a2e9ce356fa7a07c72
datyprjkt <118070774+datyprjkt@users.noreply.github.com>
2022-11-21 09:13:16 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd ${KERNEL_ROOTDIR}
+rm -rf out
 mkdir -p out
 make O=out clean
 make $KERNEL_DEFCONFIG O=out
+make menuconfig O=out
 make -j$(nproc --all) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CLANG_TRIPLE=aarch64-linux-gnu \
