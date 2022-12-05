76af78c535f617455e8d1fe990a9edadeaaafe76
datyprjkt <dhnzsyah7@gmail.com>
2022-12-05 07:05:06 +0000

    new files telegram.sh

---
+++ target.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KERNEL_ROOTDIR=/root/citrus
+KERNEL_DEFCONFIG=vendor/citrus-perf_defconfig
+CLANG_ROOTDIR=/root/proton-clang
+ANYKERNELDIR=/root/anykernel
+IMGPATH=${KERNEL_ROOTDIR}/out/arch/arm64/boot
+export KBUILD_BUILD_USER=xVoldGoadx
+export KBUILD_BUILD_HOST=xVoldGoadxCloud
+export DIR=$(pwd)
+export TG_TOKEN=5762134144:AAEqlvsY1uGq20y9Ovf3lN3ChoLrqarbH6g
+export CHAT_ID=-1001862420994
+export DATE=$(date)
+export KERNELNAME=(xVoldGoadx)
+export KERNELBRANCH=(Chime)
+export COMPILE=(Proton-Clang)
+export DEVICE=(Chime)
+
