9d182443c52f6849a918a8d105ee208a3cc1508f
datyprjkt <118070774+datyprjkt@users.noreply.github.com>
2022-12-05 13:43:56 +0700

    Delete target.sh

--- target.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-KERNEL_ROOTDIR=/root/
-KERNEL_DEFCONFIG=vendor/codename_defconfig
-CLANG_ROOTDIR=/root/proton-clang
-export KBUILD_BUILD_USER=xVoldGoadx
-export KBUILD_BUILD_HOST=xVoldGoadxCloud
